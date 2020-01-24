2531abd55ae5f55f1d688073b8f278bc026cf2db
Will Stickles <will.stickles.sensei@gmail.com>
2020-01-24 10:07:36 -0500

    Add PHPDoc blocks

--- packages/willstickles/laravelneutrino/src/Facades/Neutrino.php
+++ packages/willstickles/laravelneutrino/src/Facades/Neutrino.php
@@ -6,6 +6,11 @@ use Illuminate\SUpport\Facades\Facade;
 
 class Neutrino extends Facade
 {
+    /**
+     * Get the registered name of the component.
+     * 
+     * @return string
+     */
     protected static function getFacadeAccessor()
     {
         return 'willstickles-neutrino';
--- packages/willstickles/laravelneutrino/src/Neutrino.php
+++ packages/willstickles/laravelneutrino/src/Neutrino.php
@@ -4,6 +4,12 @@ namespace Willstickles\LaravelNeutrino;
 
 class Neutrino
 {
+    
+    /**
+     * display
+     *
+     * @return void
+     */
     public function display() 
     {
         return "This is a message from our display function";
